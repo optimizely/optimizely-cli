de64e17fde7602f4c507c8f2f533f992fadc35d8
Travis Beck <travis.beck@optimizely.com>
2017-11-21 09:17:21 -0800

    only store files when there are changes

--- optimizely_cli/commands/pull.py
+++ optimizely_cli/commands/pull.py
@@ -1,8 +1,8 @@
 import click
 import errno
+import hashlib
 import os
 import yaml
-
 
 from optimizely_cli import main
 from optimizely_cli.api import client as api_client
@@ -11,16 +11,47 @@ from optimizely_cli.api import client as api_client
 OPTIMIZELY_DATA_DIR = 'optimizely'
 
 
-def write_file_for_entity(root_dir, entity_type, entity):
-    entity_dir = os.path.join(root_dir, OPTIMIZELY_DATA_DIR, entity_type)
+def file_sha1(file_path):
+    sha1 = hashlib.sha1()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            sha1.update(chunk)
+    return sha1.hexdigest()
+
+
+def string_sha1(content):
+    sha1 = hashlib.sha1()
+    sha1.update(content)
+    return sha1.hexdigest()
+
+
+def make_directory(dir_path):
+    if os.path.isdir(dir_path):
+        return
+
     try:
-        os.makedirs(entity_dir)
+        os.makedirs(dir_path)
     except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(entity_dir):
+        if exc.errno == errno.EEXIST and os.path.isdir(dir_path):
             pass
         else:
             raise
-    path = os.path.join(entity_dir, '{}.yml'.format(entity.get('id')))
+
+
+def store_entity(root_dir, entity_type, entity):
+    content = yaml.safe_dump(entity, default_flow_style=False)
+    signature = string_sha1(content)
+
+    entity_dir = os.path.join(root_dir, OPTIMIZELY_DATA_DIR, entity_type)
+    path = os.path.join(entity_dir, '{}.yaml'.format(entity.get('id')))
+
+    if os.path.isfile(path):
+        # if the file already exists and is the same there's nothing to do
+        file_signature = file_sha1(path)
+        if file_signature == signature:
+            return
+
+    make_directory(entity_dir)
     with open(path, 'w') as outfile:
         yaml.safe_dump(entity, outfile, default_flow_style=False)
 
@@ -63,4 +94,6 @@ def pull(project, project_id):
 
     for entity_type in entities.keys():
         for entity in entities[entity_type]:
-            write_file_for_entity(project.root, entity_type, entity)
+            store_entity(project.root, entity_type, entity)
+
+    click.echo('Optimizely data is up to date')
