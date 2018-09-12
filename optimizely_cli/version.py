30abce5e440cae9484cfef8bca823c9fd4a424c4
Travis Beck <travis.beck@optimizely.com>
2018-09-12 10:41:16 -0700

    remove setup.cfg for now

--- optimizely_cli/version.py
+++ optimizely_cli/version.py
@@ -12,5 +12,5 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-version_info = (0, 2, 1)
+version_info = (0, 2, 2)
 __version__ = ".".join(str(v) for v in version_info)
